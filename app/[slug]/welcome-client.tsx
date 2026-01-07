40e38204c9e94ec7e08174a1ad54b824ee6cce2e
contact-info00 <hiwashekhjamal@gmail.com>
2026-01-07 20:34:07 +0300

    Revert "Add 3-second delay before fade-in animation on welcome page elements"
    This reverts commit 2897a18a332c4b6d71a3ed419f7d0b2482615784.

--- app/[slug]/welcome-client.tsx
+++ app/[slug]/welcome-client.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import React, { useState, useEffect } from 'react'
 import { RestaurantData } from '@/lib/get-restaurant-data'
 import { WelcomeBackground } from './welcome-background'
 
@@ -10,17 +9,6 @@ interface WelcomeClientProps {
 }
 
 export function WelcomeClient({ restaurant, children }: WelcomeClientProps) {
-  const [isLoaded, setIsLoaded] = useState(false)
-
-  useEffect(() => {
-    // Start fade-in after 3 seconds
-    const timer = setTimeout(() => {
-      setIsLoaded(true)
-    }, 3000)
-
-    return () => clearTimeout(timer)
-  }, [])
-
   const overlayStyle = {
     backgroundColor: restaurant.welcomeOverlayColor || '#000000',
     opacity: restaurant.welcomeOverlayOpacity || 0.5,
@@ -29,7 +17,7 @@ export function WelcomeClient({ restaurant, children }: WelcomeClientProps) {
   return (
     <div className="relative min-h-dvh w-full overflow-x-hidden">
       {/* Background */}
-      <WelcomeBackground restaurant={restaurant} isLoaded={isLoaded} />
+      <WelcomeBackground restaurant={restaurant} isLoaded={true} />
       
       {/* Overlay */}
       <div
@@ -39,12 +27,7 @@ export function WelcomeClient({ restaurant, children }: WelcomeClientProps) {
 
       {/* Content */}
       <div className="relative z-10 flex flex-col items-center justify-end min-h-screen p-6 pb-24">
-        {React.Children.map(children, (child) => {
-          if (React.isValidElement(child)) {
-            return React.cloneElement(child, { isLoaded } as any)
-          }
-          return child
-        })}
+        {children}
       </div>
     </div>
   )
